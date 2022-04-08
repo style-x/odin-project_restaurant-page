b1dc1d8fb40ffba7ca50e8d7852278880f2964a1
xan <47989552+style-x@users.noreply.github.com>
2022-04-08 19:57:19 +0200

    Webpack Setup #2
    wo führt das noch hin?

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 
-function component() {
+ function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  return element;
-}
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-document.body.appendChild(component());
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
